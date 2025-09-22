b33b4e3acff84711bfdf0e8156f42b65b22627f6
Jabi <michal.jabi@gmail.com>
2025-09-22 11:05:49 +0200

    task (a000): make it work.

--- JS-teoria-zadania/A-the-language-intro/a000-hello-world.js
+++ JS-teoria-zadania/A-the-language-intro/a000-hello-world.js
@@ -2,6 +2,29 @@ var welcomeMessage = 'Hello World!'
 
 console.log(welcomeMessage);
 
+console.log(20 + 1 + 2000);
+console.log(20 + 1 + 2000);
+
+function sayHello() {
+   console.log('a' + 'b')
+   return;
+}
+
+
+sayHello();
+
+90 + 10;
+
+x
+
+sayHello();
+sayHello();
+
+
+console.log(20 + 1 + 44);
+console.log(20 + 1 + 2000);
+console.log(20 + 1 + 1);
+
 // to jest prosty komentarz liniowy
 
 /*
@@ -21,3 +44,15 @@ blokowy
 // 3. uruchomienia pliku poleceniem: 
    // > node dokladna/sciezka/do/pliku/a000-hello-world.js
 // 4. zmień napis Hello World na Witaj Świecie - i uruchom plik ponownie.
+
+
+// Case:
+
+// będzie często JS
+// camelCase
+// PascalCase
+
+
+// będzie często w HTML / CSS:
+// snake_case 
+// kebab-case
