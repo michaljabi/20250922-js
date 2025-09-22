7fde02ba5d9d641c2576078d491e1e958726055e
Jabi <michal.jabi@gmail.com>
2025-09-22 15:47:05 +0200

    explain(a202): default parameters, dynamics of JS - WARN!

--- JS-teoria-zadania/A-the-language-intro/a202-functions-with-parameters.js
+++ JS-teoria-zadania/A-the-language-intro/a202-functions-with-parameters.js
@@ -1,3 +1,4 @@
+
 /**
  *  Funkcje mogą być deklarowane z tzw. parametrami.
  *  Są to zapisy odnoszące się do argumentów (danych) przekazanych do funkcji.
@@ -15,33 +16,61 @@
  */
 
 // Deklaracja:
-function sumTwoNumbers(a, b) { // a i b to parametry
-	return a + b
+function sumNumbers(a = 0, b = 0, c = 0) { // a i b to parametry
+	//console.log(a)
+	//console.log(b)
+	return a + b + c
 }
 
+console.log( sumNumbers(450, 200, 100) )
+console.log( sumNumbers(10, 20) )
+// console.log( 10 + 20 + undefined )
+console.log( sumNumbers(30, 560) )
+
+const computation = sumNumbers();
+
+console.log(computation)
+console.log(undefined + undefined);
+
+// Uważaj jakie dostarczasz Argumenty!
+/*
+const word = sumTwoNumbers('sło', 'wo')
+
+console.log(word);
+console.log('sło' / 'wo');
+*/
+
+
+// Musisz pilnować sie z ilością argumentów które przekazujesz :(
+// sumTwoNumbers(2, 3, 1,9, 3, 2, 1, 12,312, 3123, 123, 123,12 ,123);
+
+// console.log(sumTwoNumbers instanceof Function)
+// console.log(sumTwoNumbers instanceof Object)
+
 // Wywołanie:
-const result = sumTwoNumbers(10, 20) //arguments  10 i 20 to argumenty
+const result = sumNumbers(10, 20) //arguments  10 i 20 to argumenty
 
 
 // #1 Zadanie:
 // Pokaż wynik dodawania 10 + 20 na ekranie
 console.log(result)
-console.log( sumTwoNumbers(10, 20) )
+console.log( sumNumbers(10, 20) )
 
 // #2 Zadanie:
 // Policz ile to 30 + 560
-console.log( sumTwoNumbers(30, 560) )
+console.log( sumNumbers(30, 560) )
 
 // #3 Zadanie:
 // co jeśli byśmy mieli policzyć 450 + 200 + 100 ?
 
 console.log(450 + 200 + 100);
 
-console.log( sumTwoNumbers(sumTwoNumbers(450, 200), 100) )
+
+console.log( sumNumbers(sumNumbers(450, 200), 100) )
 
 
-const partialSum = sumTwoNumbers(450, 200);
-const myResult = sumTwoNumbers(partialSum, 100)
+const partialSum = sumNumbers(450, 200);
+const myResult = sumNumbers(partialSum, 100)
 console.log(myResult);
 
 /* ---------------------- */
@@ -53,7 +82,8 @@ function greetings(user = 'Admin') {
 
 // #4 Zadanie:
 // spróbuj wywołać greetings z argumentami i bez argumentu, co się stanie ?
-
+greetings()
+greetings('Michał')
 
 // #5 Zadanie:
 // Po wykonaniu zadania #4 usuń domyślną wartość user - co się stanie ?
