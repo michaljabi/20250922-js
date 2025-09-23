e8b5d8a2d9a7c03b9ef5c8bae8b51f91229a9974
Jabi <michal.jabi@gmail.com>
2025-09-23 12:59:12 +0200

    explain: for loop + solved task c103.

--- JS-teoria-zadania/C-js-with-objects/c103-iterables-in.js
+++ JS-teoria-zadania/C-js-with-objects/c103-iterables-in.js
@@ -5,10 +5,31 @@
  * */
 
 const movie = 'Ender\'s Game';
+console.log(movie[0]);
+console.log(movie.length);
 
 for(let x = 0; x < movie.length; x++) {
 	console.log(movie[x])
 }
+// console.log(x);
+
+// 
+let y = 20;
+y = y + 10; // można krócej: y += 10;
+
+// y += 4;
+console.log(y)
+
+let z = 20;
+z += 10;
+
+console.log(z);
+
+const numbers = [];
+for(let x = 2; x <= 20 ; x += 5) {
+	numbers.push(x);
+}
+console.log(numbers);
 
 /**
  * W ES6 dostajemy wygodniejszą pętle do przeliczania dowolnej kolekcji iterowalnej
@@ -21,3 +42,6 @@ for(let letter of 'Fight Club') {
 
 // #1 Zadanie:
 // Udowodnij że pętli for...of możemy również użyć do tablic
+for(const x of ['?' , '!' , '*', '😭']) {
+	console.log(x);
+}
