bbb073579474f6c580e16e96b9722d2a2d9494d9
Jabi <michal.jabi@gmail.com>
2025-09-23 09:19:57 +0200

    task(a202): phase 1 - make it work

--- JS-teoria-zadania/A-the-language-intro/a202-train-functions.js
+++ JS-teoria-zadania/A-the-language-intro/a202-train-functions.js
@@ -3,3 +3,12 @@
 // wywołana bez argumentu
 // wywołana z argumentem 'Michał' wyświetli napis: 'Witaj Michał'
 // Zastanów się nad poprawnymi nazwami funkcji i jej parametru
+
+function say(a = 'Nieznajomy') {
+    // console.log('Witaj', a, 9)
+    // console.log('Witaj ' + a + ' ' + 9)
+    console.log(`Witaj ${a}`)
+}
+
+say()
+say('Michał')
