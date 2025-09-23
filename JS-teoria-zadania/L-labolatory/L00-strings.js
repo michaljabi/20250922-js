6f44558c7bc757c74ddd53349fd5bf1a5b9b2902
Jabi <michal.jabi@gmail.com>
2025-09-23 09:28:35 +0200

    explain(L00): template strings - more...

--- JS-teoria-zadania/L-labolatory/L00-strings.js
+++ JS-teoria-zadania/L-labolatory/L00-strings.js
@@ -4,3 +4,14 @@ console.log('---')
 
 console.log(`This
         is it`);
+
+
+const name = 'Michał';
+const lastName = 'Kowalski';
+
+console.log('---')
+console.log(name, lastName);
+console.log(name + ' ' + lastName + ' lat ' + (10 + 9));
+
+console.log(`${name} ${lastName} lat ${10+9}`);
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Template_literals
