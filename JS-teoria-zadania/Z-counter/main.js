d7fd26b43d9cd1d5df1d9972006f6ffe5a5a3994
Jabi <michal.jabi@gmail.com>
2025-09-24 15:43:58 +0200

    task(z-counter): make it work

--- JS-teoria-zadania/Z-counter/main.js
+++ JS-teoria-zadania/Z-counter/main.js
@@ -2,5 +2,14 @@ console.log(' HELLO ')
 
 const btn = document.createElement('button')
 
-btn.innerText = '1'
+let n = 100;
+
+btn.innerText = n
+
+btn.addEventListener('click', () => {
+    console.log('Klik!')
+    n = n + 1;
+    btn.innerText = n
+})
+
 document.body.append(btn)
