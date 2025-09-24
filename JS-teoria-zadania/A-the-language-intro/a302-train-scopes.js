cbd6888e26b571ff826a17cd61cd440f72749a8b
Jabi <michal.jabi@gmail.com>
2025-09-24 09:54:38 +0200

    task(a302): solved - explain: let vs var and "use strict";

--- JS-teoria-zadania/A-the-language-intro/a302-train-scopes.js
+++ JS-teoria-zadania/A-the-language-intro/a302-train-scopes.js
@@ -1,3 +1,6 @@
+// "use strict";
+// 💪 DZIEŃ 3 - zadania na start: 🚀
+
 
 const helloWorld = "hello World!";
 
@@ -9,8 +12,31 @@ const helloWorld = "hello World!";
 // pomocna dłoń?
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String
 
+
+function takeFirstAndLastLetter() {
+    //  console.log(helloWorld);
+    let x = 10
+    console.log(helloWorld.at(0) + helloWorld.at(-1));
+
+    console.log(x);
+
+    // ponieważ używam `let` to poza "use strict" - nie "wycieka" x do przestrzeni globalnej (tutaj niżej sprawdzenie)
+    console.log(globalThis.x);
+
+    function show410() {
+        console.log(x + 400);
+    }
+    show410();
+}
+
+takeFirstAndLastLetter();
+
+
 // #2 Zadanie:
 // Dopisz w środku funkcji zmienną lokalną x = 10
 
 // #3 Zadanie:
 // Dopisz w środku tej funkcji inną, która skorzysta i pokaże na ekranie wartość x + 400 (410)
+
+// to jest ESM (Ecma Script Module) - powiemy o tym za chwilę, i to ma "use strict" z "automatu" !!!
+// export {};
